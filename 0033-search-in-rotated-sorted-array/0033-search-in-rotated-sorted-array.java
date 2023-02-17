49a21ec0507eead25014642a3a9403106ffb49f9
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-02-17 21:31:13 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (78.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,15 +1,55 @@
 class Solution {
     public int search(int[] nums, int target) {
      int ans=0;
-        // int mid=nums.length/2;
-        // if(target==nums[mid])
-        // {
-        //     return mid;
-        // }else if(target)
-        for (int i=0;i<nums.length;i++){
-            if(nums[i]==target)
-                return i;
+       
+        // for (int i=0;i<nums.length;i++){
+        //     if(nums[i]==target)
+        //         return i;
+        // }
+        
+//         int left = 0;
+//     int right = nums.length - 1;
+//     while (left <= right) {
+//         int mid = left + (right - left) / 2;
+//         if (nums[mid] == target) {
+//             return mid;
+//         }
+//        if(nums[left]<=nums[mid]){
+//            if(target>nums[left]&&target<=nums[mid]){
+//                left=mid-1;
+//            }else{
+//                right=mid+1;
+//            }
+//        }else{
+//            if(target<nums[right]&&target>=nums[mid]){
+//                 left=mid+1;
+//            }else{
+//                right=mid-1;
+//        }
+//     }
+// }
+//         return -1;
+        int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
         }
-        return -1;
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
     }
 }
