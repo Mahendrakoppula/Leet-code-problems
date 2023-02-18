7f3dc9cd5118193315164623167b9b1f31ea6900
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-02-18 23:48:28 +0530

    Time: 30 ms (64.25%), Space: 60 MB (72.92%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -11,23 +11,39 @@ class Solution {
 //             }
         
         // return count;
-  Arrays.sort(nums);
-        return smaller(nums, upper) - smaller(nums, --lower);
-    }
+//   Arrays.sort(nums);
+//         return smaller(nums, upper) - smaller(nums, --lower);
+//     }
 
-    private long smaller(int[] nums, int value) {
-        int l = 0, r = nums.length - 1;
-        long result = 0;
-        while (l < r) {
-            int sum = nums[l] + nums[r];
-            if (sum <= value) {
-                result += (r - l);
-                l++;    
-            } else {   
-                r--;
-            }
-        }
+//     private long smaller(int[] nums, int value) {
+//         int l = 0, r = nums.length - 1;
+//         long result = 0;
+//         while (l < r) {
+//             int sum = nums[l] + nums[r];
+//             if (sum <= value) {
+//                 result += (r - l);
+//                 l++;    
+//             } else {   
+//                 r--;
+//             }
+//         }
             
-        return result;
+//         return result;
+        long res = 0;
+        Arrays.sort(nums);
+        int idxL = nums.length - 1, idxR = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            idxL = Math.max(idxL, i + 1);
+            while (idxL - 1 > i && nums[idxL - 1] + nums[i] >= lower)
+                idxL--;
+
+            while (idxR > 0 && nums[idxR] + nums[i] > upper)
+                idxR--;
+
+            if (idxL <= idxR && nums[i] + nums[idxL] >= lower && nums[i] + nums[idxR] <= upper)
+                res += idxR - idxL + 1;
+        }
+        return res;
 }
 }
