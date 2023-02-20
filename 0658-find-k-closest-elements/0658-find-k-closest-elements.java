e23e7cfc5779a51af45858bcd5a000a11a96e4f3
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-02-20 21:39:21 +0530

    Time: 32 ms (22.42%), Space: 44.8 MB (22.68%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,5 +1,5 @@
 class Solution {
-//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
 //         List<Integer>result=new ArrayList<>();
         
 //         for(int i=0;i<=arr.length;i++){
@@ -27,27 +27,56 @@ class Solution {
 //         return result;
 //     }
     
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-    List<Integer> result = new ArrayList<>();
+    // public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    // List<Integer> result = new ArrayList<>();
 
-    // Create a list of pairs (distance, element) for each element in the array
-    List<Pair<Integer, Integer>> distances = new ArrayList<>();
+//     // Create a list of pairs (distance, element) for each element in the array
+//     List<Pair<Integer, Integer>> distances = new ArrayList<>();
+//     for (int i = 0; i < arr.length; i++) {
+//         distances.add(new Pair<>(Math.abs(arr[i] - x), arr[i]));
+//     }
+
+//     // Sort the list based on distances
+//     distances.sort((a, b) -> a.getKey() - b.getKey());
+
+//     // Take the first k elements from the sorted list
+//     for (int i = 0; i < k; i++) {
+//         result.add(distances.get(i).getValue());
+//     }
+
+//     // Sort the result in ascending order
+//     result.sort((a, b) -> a - b);
+
+//     return result;
+// }
+    
+      List<List<Integer>> result = new ArrayList<>();
+
+    List<int[]> distances = new ArrayList<>();
     for (int i = 0; i < arr.length; i++) {
-        distances.add(new Pair<>(Math.abs(arr[i] - x), arr[i]));
+        int[] pair = new int[2];
+        pair[0] = arr[i];
+        pair[1] = Math.abs(arr[i] - x);
+        distances.add(pair);
     }
 
-    // Sort the list based on distances
-    distances.sort((a, b) -> a.getKey() - b.getKey());
+    distances.sort(Comparator.comparingInt(a -> a[1]));
 
-    // Take the first k elements from the sorted list
     for (int i = 0; i < k; i++) {
-        result.add(distances.get(i).getValue());
+        List<Integer> temp = new ArrayList<>();
+        temp.add(distances.get(i)[0]);
+        result.add(temp);
     }
 
-    // Sort the result in ascending order
-    result.sort((a, b) -> a - b);
+    result.sort(Comparator.comparingInt(a -> a.get(0)));
+    
+    // Flatten the nested list of lists into a single list of integers
+    List<Integer> flattenedResult = new ArrayList<>();
+    for (List<Integer> sublist : result) {
+        flattenedResult.addAll(sublist);
+    }
 
-    return result;
-}
+    return flattenedResult;
+    }
 
 }
