38f34ddfa3696acbca7b006b5726cb2b85040687
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-02-21 16:15:03 +0530

    Time: 0 ms (100.00%), Space: 48 MB (85.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -23,22 +23,62 @@ class Solution {
 //             }
 //             return result;
         
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (freq.containsKey(nums[i])) {
-              int count = freq.get(nums[i]) + 1;
-                freq.put(nums[i], count);
-            } else {
-                freq.put(nums[i], 1);
-            }
-        }
-        int c=0;
-        for (Map.Entry<Integer, Integer> i : freq.entrySet()) {
-            if (i.getValue() == 1)
-                c= i.getKey();
+//         HashMap<Integer, Integer> freq = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             if (freq.containsKey(nums[i])) {
+//               int count = freq.get(nums[i]) + 1;
+//                 freq.put(nums[i], count);
+//             } else {
+//                 freq.put(nums[i], 1);
+//             }
+//         }
+//         int c=0;
+//         for (Map.Entry<Integer, Integer> i : freq.entrySet()) {
+//             if (i.getValue() == 1)
+//                 c= i.getKey();
 
-        }
-        return c;
-            }
-        }
+//         }
+//         return c;
+//             }
+//         }
         
+        int start = 0, end = nums.length - 1, mid;
+ 
+        // For Edge Cases
+        if (nums.length
+            == 1) // If only one element is in the array
+            return nums[0];
+ 
+        if (nums[start]
+            != nums[start + 1]) // If the first element
+                                // is the element that
+                                // appears only once
+            return nums[start];
+ 
+        if (nums[end]
+            != nums[end
+                    - 1]) // If Last element is the element
+                          // that appears only once
+            return nums[end];
+ 
+        // Binary Search
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            // CASE 1
+            if (nums[mid] != nums[mid - 1]
+                && nums[mid] != nums[mid + 1])
+                return nums[mid];
+            // CASE 2 and CASE 3
+            else if ((nums[mid] == nums[mid + 1]
+                      && mid % 2 == 0)
+                     || (nums[mid] == nums[mid - 1]
+                         && mid % 2 != 0))
+                start = mid + 1;
+            // CASE 4 and CASE 5
+            else
+                end = mid - 1;
+        }
+        // If no such element found
+        return -1;
+    }
+}
