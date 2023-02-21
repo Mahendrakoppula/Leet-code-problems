dddf87f276aef57d36130d06e157bc371048c580
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-02-21 19:52:21 +0530

    Time: 7 ms (9.52%), Space: 42.2 MB (76.49%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
@@ -1,41 +1,34 @@
 class Solution {
     public int findClosestNumber(int[] nums) {
         Arrays.sort(nums);
-        int lo=0;
-        int hi=nums.length-1;
-        int mid=0;
-        int close=nums[0];
+//         int lo=0;
+//         int hi=nums.length-1;
+//         int mid=0;
+//         int close=nums[0];
         
-        while(lo<=hi){
-            mid=lo+(hi-lo)/2;
-            if(nums[mid]>=0){
-                hi=mid-1;
-            }else{
-                lo=mid+1;
-            }
-            if (Math.abs(nums[mid])<Math.abs(close)){
-                close=nums[mid];
-            }else if (Math.abs(nums[mid])==Math.abs(close)){
-                close=Math.max(close,nums[mid]);
-            }  
+//         while(lo<=hi){
+//             mid=lo+(hi-lo)/2;
+//             if(nums[mid]>=0){
+//                 hi=mid-1;
+//             }else{
+//                 lo=mid+1;
+//             }
+//             if (Math.abs(nums[mid])<Math.abs(close)){
+//                 close=nums[mid];
+//             }else if (Math.abs(nums[mid])==Math.abs(close)){
+//                 close=Math.max(close,nums[mid]);
+//             }  
+//         }
+//         return close;
+        
+      int closest = nums[0];
+    for (int i = 0; i < nums.length; i++) {
+        if (Math.abs(nums[i]) < Math.abs(closest)) {
+            closest = nums[i];
+        } else if (Math.abs(nums[i]) == Math.abs(closest)) {
+            closest = Math.max(closest, nums[i]);
         }
-        return close;
-        // Arrays.sort(nums);
-        // int left = 0, right = nums.length - 1;
-        // int closest = nums[0];
-        // while (left <= right) {
-        //     int mid = (left + right) / 2;
-        //     if (nums[mid] >= 0) {
-        //         right = mid - 1;
-        //     } else {
-        //         left = mid + 1;
-        //     }
-        //     if (Math.abs(nums[mid]) < Math.abs(closest)) {
-        //         closest = nums[mid];
-        //     } else if (Math.abs(nums[mid]) == Math.abs(closest)) {
-        //         closest = Math.max(closest, nums[mid]);
-        //     }
-        // }
-        // return closest;
+    }
+    return closest;
     }
     }
