5c0ca5076d29719f58e3632d977223ef4ecb4354
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-02-22 23:05:55 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.85%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numberOfMatches(int n) {
+         if (n == 1) return 0;
+        
+		// We declare an int to hold our recursive solution.
+        int res;
+		
+		// We initialize res using a recursive call, reducing n 
+		// as described in the problem.
+        if (n % 2 == 0) {
+            res = numberOfMatches(n / 2);
+			// After the recursive call is executed, we add the appropriate value to 
+			// our solution variable.
+            res += n / 2;
+        }
+        else {
+            res = numberOfMatches((n - 1) / 2 + 1);
+            res += (n - 1) / 2;
+        }
+        
+		// Our initial call to numberOfMatches()
+		// will return the total number of matches 
+		// added to res in each recursive call.
+        return res;
+    }
+}
