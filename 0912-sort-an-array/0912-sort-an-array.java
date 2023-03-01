2ee190a6d5e620de26dce957c9f4ff370acdd7e9
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-01 19:34:53 +0530

    Time: 4 ms (99.26%), Space: 51.6 MB (91.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int[] sortArray(int[] arr) {
+    public int[] sortArray(int[] nums) {
+    // public int[] sortArray(int[] arr) {
 //         int n = arr.length;
 //     if (n <= 1) {
 //         return arr; // Nothing to sort
@@ -40,90 +41,113 @@ class Solution {
     
 //     return result;
 //     }
-         int n = arr.length;
-    int minRun = minRunLength(n);
+//          int n = arr.length;
+//     int minRun = minRunLength(n);
     
-    // Divide the input array into runs of minRun length and sort each run using insertion sort
-    for (int i = 0; i < n; i += minRun) {
-        int end = Math.min(i + minRun - 1, n - 1);
-        insertionSort(arr, i, end);
-    }
+//     // Divide the input array into runs of minRun length and sort each run using insertion sort
+//     for (int i = 0; i < n; i += minRun) {
+//         int end = Math.min(i + minRun - 1, n - 1);
+//         insertionSort(arr, i, end);
+//     }
     
-    // Merge adjacent runs until only one run remains
-    for (int size = minRun; size < n; size = 2 * size) {
-        for (int left = 0; left < n; left += 2 * size) {
-            int mid = left + size - 1;
-            int right = Math.min(left + 2 * size - 1, n - 1);
-            merge(arr, left, mid, right);
-        }
-    }
+//     // Merge adjacent runs until only one run remains
+//     for (int size = minRun; size < n; size = 2 * size) {
+//         for (int left = 0; left < n; left += 2 * size) {
+//             int mid = left + size - 1;
+//             int right = Math.min(left + 2 * size - 1, n - 1);
+//             merge(arr, left, mid, right);
+//         }
+//     }
     
-    return arr;
-}
+//     return arr;
+// }
 
-private static int minRunLength(int n) {
-    int r = 0;
-    while (n >= 64) {
-        r |= (n & 1);
-        n >>= 1;
-    }
-    return n + r;
-}
+// private static int minRunLength(int n) {
+//     int r = 0;
+//     while (n >= 64) {
+//         r |= (n & 1);
+//         n >>= 1;
+//     }
+//     return n + r;
+// }
 
-private static void insertionSort(int[] arr, int left, int right) {
-    for (int i = left + 1; i <= right; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= left && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+// private static void insertionSort(int[] arr, int left, int right) {
+//     for (int i = left + 1; i <= right; i++) {
+//         int key = arr[i];
+//         int j = i - 1;
+//         while (j >= left && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
+//         arr[j + 1] = key;
+//     }
+// }
+
+// private static void merge(int[] arr, int left, int mid, int right) {
+//     int len1 = mid - left + 1;
+//     int len2 = right - mid;
+
+//     // Check for negative array sizes
+//     if (len1 < 0 || len2 < 0) {
+//         return;
+//     }
+
+//     int[] leftArr = new int[len1];
+//     int[] rightArr = new int[len2];
+
+//     for (int i = 0; i < len1; i++) {
+//         leftArr[i] = arr[left + i];
+//     }
+//     for (int j = 0; j < len2; j++) {
+//         rightArr[j] = arr[mid + 1 + j];
+//     }
+
+//     int i = 0, j = 0, k = left;
+
+//     while (i < len1 && j < len2) {
+//         if (leftArr[i] <= rightArr[j]) {
+//             arr[k] = leftArr[i];
+//             i++;
+//         } else {
+//             arr[k] = rightArr[j];
+//             j++;
+//         }
+//         k++;
+//     }
+
+//     while (i < len1) {
+//         arr[k] = leftArr[i];
+//         i++;
+//         k++;
+//     }
+
+//     while (j < len2) {
+//         arr[k] = rightArr[j];
+//         j++;
+//         k++;
+//     }
+        
+         int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
+        for (int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
         }
-        arr[j + 1] = key;
-    }
-}
-
-private static void merge(int[] arr, int left, int mid, int right) {
-    int len1 = mid - left + 1;
-    int len2 = right - mid;
-
-    // Check for negative array sizes
-    if (len1 < 0 || len2 < 0) {
-        return;
-    }
-
-    int[] leftArr = new int[len1];
-    int[] rightArr = new int[len2];
-
-    for (int i = 0; i < len1; i++) {
-        leftArr[i] = arr[left + i];
-    }
-    for (int j = 0; j < len2; j++) {
-        rightArr[j] = arr[mid + 1 + j];
-    }
-
-    int i = 0, j = 0, k = left;
-
-    while (i < len1 && j < len2) {
-        if (leftArr[i] <= rightArr[j]) {
-            arr[k] = leftArr[i];
-            i++;
-        } else {
-            arr[k] = rightArr[j];
-            j++;
+        int len = max - min;
+        int[] arr = new int[len + 1];
+        for (int num : nums) {
+            arr[num - min]++;
         }
-        k++;
+        int index = 0;
+        for (int i = 0; i <= len; i++) {
+            while (arr[i] > 0) {
+                nums[index] = min;
+                index++;
+                arr[i]--;
+            }
+            min++;
+        }
+        return nums;
     }
-
-    while (i < len1) {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
-    }
-
-    while (j < len2) {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
-    }
-}
 }
