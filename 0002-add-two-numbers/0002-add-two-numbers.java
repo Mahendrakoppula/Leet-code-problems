7e25f2b543c48495177e28c749335c6b7163cb3e
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-02 17:35:47 +0530

    Time: 2 ms (95.73%), Space: 43.2 MB (19.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,26 +9,56 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    int carry = 0;
-    ListNode dummy = new ListNode(0);
-    ListNode curr = dummy;
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//     int carry = 0;
+//     ListNode dummy = new ListNode(0);
+//     ListNode curr = dummy;
 
-    while (l1 != null || l2 != null) {
-        int x = (l1 != null) ? l1.val : 0;
-        int y = (l2 != null) ? l2.val : 0;
-        int sum = x + y + carry;
-        carry = sum / 10;
-        curr.next = new ListNode(sum % 10);
-        curr = curr.next;
-        if (l1 != null) l1 = l1.next;
-        if (l2 != null) l2 = l2.next;
+//     while (l1 != null || l2 != null) {
+//         int x = (l1 != null) ? l1.val : 0;
+//         int y = (l2 != null) ? l2.val : 0;
+//         int sum = x + y + carry;
+//         carry = sum / 10;
+//         curr.next = new ListNode(sum % 10);
+//         curr = curr.next;
+//         if (l1 != null) l1 = l1.next;
+//         if (l2 != null) l2 = l2.next;
+//     }
+
+//     if (carry > 0) {
+//         curr.next = new ListNode(carry);
+//     }
+
+//     return dummy.next;
+// }
+    
+   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode result = null;
+    ListNode curr = null;
+    int sum = 0;
+    
+    // Calculate the sum of the digits in reverse order
+    while (l1 != null || l2 != null || sum > 0) {
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        if (result == null) {
+            result = new ListNode(sum % 10);
+            curr = result;
+        } else {
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+        sum /= 10;
     }
-
-    if (carry > 0) {
-        curr.next = new ListNode(carry);
-    }
-
-    return dummy.next;
+    
+    return result;
 }
+
+
 }
