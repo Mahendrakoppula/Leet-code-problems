bf0917ba2a5fe27d6a34d58de9e0fc0eb7ac6879
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-03 18:20:50 +0530

    Time: 2 ms (95.83%), Space: 43.1 MB (21.36%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+// class Solution {
 //     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 //     int carry = 0;
 //     ListNode dummy = new ListNode(0);
@@ -31,34 +31,57 @@ class Solution {
 
 //     return dummy.next;
 // }
-    
-   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode result = null;
-    ListNode curr = null;
-    int sum = 0;
-    
-    // Calculate the sum of the digits in reverse order
-    while (l1 != null || l2 != null || sum > 0) {
-        if (l1 != null) {
-            sum += l1.val;
-            l1 = l1.next;
+    class Solution {
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode result=null;
+     ListNode curr=null;
+     int sum=0;
+    while(l1!=null||l2!=null||sum>0){
+        if(l1!=null){
+            sum+=l1.val;
+            l1=l1.next;
         }
-        if (l2 != null) {
-            sum += l2.val;
-            l2 = l2.next;
+        if(l2!=null){
+            sum+=l2.val;
+            l2=l2.next;
         }
-        if (result == null) {
-            result = new ListNode(sum % 10);
-            curr = result;
-        } else {
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
+        if(result==null){
+            result=new ListNode(sum%10);
+            curr=result;
+        }else{
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
         }
-        sum /= 10;
-    }
-    
-    return result;
+    sum/=10;
+    } 
+       return result;
 }
-
-
-}
+ }
+//          public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//     ListNode result = null;
+//     ListNode curr = null;
+//     int sum = 0;
+    
+//     // Calculate the sum of the digits in reverse order
+//     while (l1 != null || l2 != null || sum > 0) {
+//         if (l1 != null) {
+//             sum += l1.val;
+//             l1 = l1.next;
+//         }
+//         if (l2 != null) {
+//             sum += l2.val;
+//             l2 = l2.next;
+//         }
+//         if (result == null) {
+//             result = new ListNode(sum % 10);
+//             curr = result;
+//         } else {
+//             curr.next = new ListNode(sum % 10);
+//             curr = curr.next;
+//         }
+//         sum /= 10;
+//     }
+    
+//     return result;
+// }
+// }
