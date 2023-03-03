a63dd7d36321f89896dc4be058c1bbd258514d47
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-03 20:35:15 +0530

    Time: 2 ms (13.69%), Space: 40.8 MB (22.79%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        if (name.length() > typed.length()) {
+            return false;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        int i = 0, j = 0;
+        
+        while (i < name.length() && j < typed.length()) {
+            char c1 = name.charAt(i);
+            char c2 = typed.charAt(j);
+            
+            if (c1 != c2) {
+                if (stack.isEmpty() || stack.peek() != c2) {
+                    return false;
+                }
+                while (j < typed.length() && typed.charAt(j) == c2) {
+                    j++;
+                }
+            } else {
+                stack.push(c2);
+                i++;
+                j++;
+            }
+        }
+        
+        while (j < typed.length()) {
+            if (stack.isEmpty() || stack.peek() != typed.charAt(j)) {
+                return false;
+            }
+            j++;
+        }
+        
+        return i == name.length() && j == typed.length();
+    }
+}
