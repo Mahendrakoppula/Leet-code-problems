b6f40c669ed8c3e9c7db539f3e8c075c72f95fb1
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-03 18:43:05 +0530

    Time: 20 ms (11.37%), Space: 42.6 MB (46.83%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num3) {
+        // in1=Integer.parseInt(num1);
+        // int n3=Integer.parseInt(num3);
+        BigInteger n1= new BigInteger(num1);
+        BigInteger n3 = new BigInteger(num3);
+        BigInteger sum= n1.add(n3);
+       // int sum=n1+n3;
+        // String result=sum.toString(sum);
+        
+        return sum.toString();
+    }
+}
