0b837c6b000969ba67ef2f563eacc16954d79958
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-03 20:39:32 +0530

    Time: 2 ms (13.69%), Space: 40.5 MB (48.96%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -1,37 +1,65 @@
 class Solution {
-    public boolean isLongPressedName(String name, String typed) {
-        if (name.length() > typed.length()) {
-            return false;
-        }
+//     public boolean isLongPressedName(String name, String typed) {
+//         if (name.length() > typed.length()) {
+//             return false;
+//         }
         
+//         Stack<Character> stack = new Stack<>();
+//         int i = 0, j = 0;
+        
+//         while (i < name.length() && j < typed.length()) {
+//             char c1 = name.charAt(i);
+//             char c2 = typed.charAt(j);
+            
+//             if (c1 != c2) {
+//                 if (stack.isEmpty() || stack.peek() != c2) {
+//                     return false;
+//                 }
+//                 while (j < typed.length() && typed.charAt(j) == c2) {
+//                     j++;
+//                 }
+//             } else {
+//                 stack.push(c2);
+//                 i++;
+//                 j++;
+//             }
+//         }
+        
+//         while (j < typed.length()) {
+//             if (stack.isEmpty() || stack.peek() != typed.charAt(j)) {
+//                 return false;
+//             }
+//             j++;
+//         }
+        
+//         return i == name.length() && j == typed.length();
+//     }
+    
+       public boolean isLongPressedName(String name, String typed) {
         Stack<Character> stack = new Stack<>();
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
         
         while (i < name.length() && j < typed.length()) {
-            char c1 = name.charAt(i);
-            char c2 = typed.charAt(j);
-            
-            if (c1 != c2) {
-                if (stack.isEmpty() || stack.peek() != c2) {
-                    return false;
-                }
-                while (j < typed.length() && typed.charAt(j) == c2) {
-                    j++;
-                }
-            } else {
-                stack.push(c2);
+            if (name.charAt(i) == typed.charAt(j)) {
+                stack.push(typed.charAt(j));
                 i++;
                 j++;
+            } else if (!stack.isEmpty() && stack.peek() == typed.charAt(j)) {
+                j++;
+            } else {
+                return false;
             }
         }
         
         while (j < typed.length()) {
-            if (stack.isEmpty() || stack.peek() != typed.charAt(j)) {
+            if (!stack.isEmpty() && stack.peek() == typed.charAt(j)) {
+                j++;
+            } else {
                 return false;
             }
-            j++;
         }
         
-        return i == name.length() && j == typed.length();
+        return i == name.length();
     }
 }
