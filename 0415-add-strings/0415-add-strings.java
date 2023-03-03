f8444d712c9b272cefba99a5b38d1636382902a6
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-03 20:16:32 +0530

    Time: 2 ms (87.77%), Space: 42.6 MB (46.83%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,14 +1,27 @@
 import java.math.BigInteger;
 class Solution {
-    public String addStrings(String num1, String num3) {
-        // in1=Integer.parseInt(num1);
-        // int n3=Integer.parseInt(num3);
-        BigInteger n1= new BigInteger(num1);
-        BigInteger n3 = new BigInteger(num3);
-        BigInteger sum= n1.add(n3);
-       // int sum=n1+n3;
-        String result=sum.toString();
+    // public String addStrings(String num1, String num3) {
         
-        return result;
+//         BigInteger n1= new BigInteger(num1);
+//         BigInteger n3 = new BigInteger(num3);
+//         BigInteger sum= n1.add(n3);
+//        // int sum=n1+n3;
+//         String result=sum.toString();
+        
+//         return result;
+    public String addStrings(String num1, String num2) {
+    int i = num1.length() - 1, j = num2.length() - 1;
+    int carry = 0;
+    StringBuilder sb = new StringBuilder();
+    while (i >= 0 || j >= 0 || carry > 0) {
+        int x = i >= 0 ? num1.charAt(i--) - '0' : 0;
+        int y = j >= 0 ? num2.charAt(j--) - '0' : 0;
+        int sum = x + y + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        sb.append(sum);
     }
+    return sb.reverse().toString();
 }
+
+    }
