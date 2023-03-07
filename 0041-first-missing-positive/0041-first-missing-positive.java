00e51f40a85df20c79c47c841374e7773747f094
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-07 21:15:31 +0530

    Time: 1 ms (99.98%), Space: 51.3 MB (27.37%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -17,5 +17,20 @@ class Solution {
     
     // All positive integers are present, so return the next integer
     return nums.length+1;
+        
+    //          Set<Integer> set = new HashSet<Integer>();
+    // int max = 0;
+    // for (int num : nums) {
+    //     if (num > 0) {
+    //         set.add(num);
+    //         max = Math.max(max, num);
+    //     }
+    // }
+    // for (int i = 1; i <= max + 1; i++) {
+    //     if (!set.contains(i)) {
+    //         return i;
+    //     }
+    // }
+    // return 1;
     }
 }
