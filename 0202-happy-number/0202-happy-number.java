a0290214c0a6dc3f442cdeeeb62a5a18bc431ef9
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-10 15:51:54 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (67.21%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,25 +1,41 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1 ){
+    // if (n == 1) {
+    //     return true;
+    // }
+    
+    // boolean[] visited=new boolean[1000];
+    // int[] squares={0,1,4,9,16,25,36,49,64,81};
+
+
+    // while(n!=1){
+    //     int sum=0;
+    //     while(n>0){
+    //         int digit=n%10;
+    //         sum+=squares[digit];
+    //         n/=10;
+    //     }
+    //     if(visited[sum]){
+    //         return false;
+    //     }
+
+    //     visited[sum]=true;
+    //     n=sum;
+    // }
+    // return true;
+    if(n==1){
             return true;
         }
-         Set<Integer> st= new HashSet<>();
-        while(n!=1){
-            int sum=0;
-            while(n>0){
-                int digit= n%10;
-                sum+=digit*digit;
-                n/=10;
-            }
-            if(st.contains(sum)){
-                return false;
-            }
-            else{
-                st.add(sum);
-                n=sum;
-            }
+        if(n>1 && n<=4){
+            return false;
         }
-        return true;
-        
-    }
+         int res =0;
+        while(n>0){
+            int v = n%10;
+            res+=(v*v);
+            n=n/10;
+        }
+       
+        return isHappy(res);
+}
 }
