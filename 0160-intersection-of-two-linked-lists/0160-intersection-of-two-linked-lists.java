4ac259029da50927aef5a70595431e50c21eddb4
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-10 21:21:11 +0530

    Time: 1 ms (98.40%), Space: 44.8 MB (90.92%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -24,51 +24,51 @@ public class Solution {
     //     }
     //     return null;
         
-//     int lenA = getLength(headA);
-//     int lenB = getLength(headB);
-//     ListNode A = headA, B = headB;
+    int lenA = getLength(headA);
+    int lenB = getLength(headB);
+    ListNode A = headA, B = headB;
     
-//     while (lenA > lenB) {
-//         A = A.next;
-//         lenA--;
-//     }
-//     while (lenB > lenA) {
-//         B = B.next;
-//         lenB--;
-//     }
+    while (lenA > lenB) {
+        A = A.next;
+        lenA--;
+    }
+    while (lenB > lenA) {
+        B = B.next;
+        lenB--;
+    }
     
-//     while (A != null && B != null) {
-//         if (A == B) {
-//             return A;
+    while (A != null && B != null) {
+        if (A == B) {
+            return A;
+        }
+        A = A.next;
+        B = B.next;
+    }
+    return null;
+}
+    private int getLength(ListNode node) {
+    int len = 0;
+    while (node != null) {
+        len++;
+        node = node.next;
+    }
+    return len;
+        
+//         Set<ListNode> st= new HashSet<>();
+        
+//         ListNode a=headA;
+//         while(a!=null){
+//             st.add(a);
+//             a=a.next;
 //         }
-//         A = A.next;
-//         B = B.next;
-//     }
-//     return null;
-// }
-//     private int getLength(ListNode node) {
-//     int len = 0;
-//     while (node != null) {
-//         len++;
-//         node = node.next;
-//     }
-//     return len;
         
-        Set<ListNode> st= new HashSet<>();
-        
-        ListNode a=headA;
-        while(a!=null){
-            st.add(a);
-            a=a.next;
-        }
-        
-        ListNode b=headB;
-        while(b!=null){
-           if(st.contains(b)){
-               return b;
-           }
-            b=b.next;
-        }
-        return null;
+//         ListNode b=headB;
+//         while(b!=null){
+//            if(st.contains(b)){
+//                return b;
+//            }
+//             b=b.next;
+//         }
+//         return null;
     }
 }
