2ca40e48cf391d051cb5ba31d104395e48905cb9
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-14 19:56:52 +0530

    Time: 6 ms (86.49%), Space: 42.8 MB (62.84%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -17,22 +17,44 @@ class Solution {
 //         }
 //         return -1;
         
+    //     int i = 0;
+    // while (i < s.length()) {
+    //     int j = 0;
+    //     boolean isUnique = true;
+    //     while (j < s.length()) {
+    //         if (i != j && s.charAt(i) == s.charAt(j)) {
+    //             isUnique = false;
+    //             break;
+    //         }
+    //         j++;
+    //     }
+    //     if (isUnique) {
+    //         return i;
+    //     }
+    //     i++;
+    // }
+    // return -1;
+        
+        
+        int n = s.length();
+        int[] freq = new int[26]; // Assuming only lowercase English letters
+        
+        // Store the frequency of each character in the string
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            freq[c - 'a']++;
+        }
+        
+        // Find the index of the first non-repeating character
         int i = 0;
-    while (i < s.length()) {
-        int j = 0;
-        boolean isUnique = true;
-        while (j < s.length()) {
-            if (i != j && s.charAt(i) == s.charAt(j)) {
-                isUnique = false;
-                break;
+        while (i < n) {
+            char c = s.charAt(i);
+            if (freq[c - 'a'] == 1) {
+                return i;
             }
-            j++;
+            i++;
         }
-        if (isUnique) {
-            return i;
-        }
-        i++;
-    }
-    return -1;
+        
+        return -1; // No non-repeating character found
     }
 }
