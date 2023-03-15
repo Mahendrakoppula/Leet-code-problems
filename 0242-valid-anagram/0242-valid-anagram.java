8cb5d57438609efc009f78666d06472c9b2aa68e
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-15 16:43:07 +0530

    Time: 18 ms (17.78%), Space: 43.3 MB (17.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String ransomNote, String magazine) {
+        
+        if(ransomNote.length()!=magazine.length()){
+            return false;
+        }
+        
+        
+         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        
+        // Count the frequency of characters in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        // Check if we can construct ransomNote using magazine
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            if (!map.containsKey(c) || map.get(c) <= 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+        
+        return true;
+    }
+}
