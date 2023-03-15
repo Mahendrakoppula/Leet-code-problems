3071ef0670c24106e1a9a00189454c3a264f3706
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-15 16:44:55 +0530

    Time: 12 ms (31.75%), Space: 42.8 MB (38.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,23 +6,43 @@ class Solution {
         }
         
         
-         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+//          HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         
-        // Count the frequency of characters in magazine
-        for (int i = 0; i < magazine.length(); i++) {
-            char c = magazine.charAt(i);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
+//         // Count the frequency of characters in magazine
+//         for (int i = 0; i < magazine.length(); i++) {
+//             char c = magazine.charAt(i);
+//             map.put(c, map.getOrDefault(c, 0) + 1);
+//         }
         
-        // Check if we can construct ransomNote using magazine
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char c = ransomNote.charAt(i);
-            if (!map.containsKey(c) || map.get(c) <= 0) {
-                return false;
+//         // Check if we can construct ransomNote using magazine
+//         for (int i = 0; i < ransomNote.length(); i++) {
+//             char c = ransomNote.charAt(i);
+//             if (!map.containsKey(c) || map.get(c) <= 0) {
+//                 return false;
+//             }
+//             map.put(c, map.get(c) - 1);
+//         }
+        
+//         return true;
+        
+         Stack<Character>st= new Stack<>();
+        
+        int[] freq = new int[26];
+    for (char c : magazine.toCharArray()) {
+        freq[c - 'a']++;
+    }
+
+    for (char c : ransomNote.toCharArray()) {
+        if (freq[c - 'a'] > 0) {
+            freq[c - 'a']--;
+            if (!st.contains(c)) {
+                st.push(c);
             }
-            map.put(c, map.get(c) - 1);
+        } else {
+            return false;
         }
-        
-        return true;
+    }
+
+    return true;
     }
 }
