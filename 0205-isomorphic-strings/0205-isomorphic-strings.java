91986d1a5a5b5baafd91c2a33d7d9673997e8062
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-16 20:23:38 +0530

    Time: 10 ms (59.58%), Space: 42.5 MB (35.34%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,44 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> mapping = new HashMap<>();
-    Set<Character> usedChars = new HashSet<>();
+//         Map<Character, Character> mapping = new HashMap<>();
+//     Set<Character> usedChars = new HashSet<>();
     
-    for (int i = 0; i < s.length(); i++) {
-        char sChar = s.charAt(i);
-        char tChar = t.charAt(i);
+//     for (int i = 0; i < s.length(); i++) {
+//         char sChar = s.charAt(i);
+//         char tChar = t.charAt(i);
         
-        if (mapping.containsKey(sChar)) {
-            if (mapping.get(sChar) != tChar) {
+//         if (mapping.containsKey(sChar)) {
+//             if (mapping.get(sChar) != tChar) {
+//                 return false;
+//             }
+//         } else {
+//             if (usedChars.contains(tChar)) {
+//                 return false;
+//             }
+//             mapping.put(sChar, tChar);
+//             usedChars.add(tChar);
+//         }
+//     }
+    
+//     return true;
+        
+         if (s == null || t == null || s.length() != t.length()) {
+        return false;
+    }
+    
+    Map<Character, Character> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+        char c1 = s.charAt(i);
+        char c2 = t.charAt(i);
+        if (map.containsKey(c1)) {
+            if (map.get(c1) != c2) {
                 return false;
             }
         } else {
-            if (usedChars.contains(tChar)) {
+            if (map.containsValue(c2)) {
                 return false;
             }
-            mapping.put(sChar, tChar);
-            usedChars.add(tChar);
+            map.put(c1, c2);
         }
     }
     
