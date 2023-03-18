1916fbcf51863679d88b35e80c799af741b98579
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-18 13:04:56 +0530

    Time: 2 ms (45.94%), Space: 43.9 MB (49.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,30 +1,31 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        int j=0;
+//         int i=0;
+//         int j=0;
         
-        while(j<nums.length){
-            if(nums[j]!=0){
-                nums[i]=nums[j];
-                i++;
-            }
-            j++;
-        }
+//         while(j<nums.length){
+//             if(nums[j]!=0){
+//                 nums[i]=nums[j];
+//                 i++;
+//             }
+//             j++;
+//         }
         
-        for(int k=i;k<nums.length;k++){
-            nums[k]=0;
-        }
-        System.out.println(Arrays.toString(nums));
+//         for(int k=i;k<nums.length;k++){
+//             nums[k]=0;
+//         }
+//         System.out.println(Arrays.toString(nums));
     
-    }
-        //   int size = 0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==0){
-        //         size++;
-        //     }else if(size>0){
-        //         int t = nums[i];
-        //         nums[i] = 0;
-        //         nums[i-size] = t;
-        //     }
-        // }
+//     }
+          int size = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                size++;
+            }else if(size>0){
+                int t = nums[i];
+                nums[i] = 0;
+                nums[i-size] = t;
+            }
+        }
+}
 }
