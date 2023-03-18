35727487d51c74a4d24e2ac8856effda486b4eb7
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-18 13:03:22 +0530

    Time: 13 ms (14.95%), Space: 44.4 MB (14.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -14,30 +14,17 @@ class Solution {
         for(int k=i;k<nums.length;k++){
             nums[k]=0;
         }
-        // while(i<nums.length){
-        //     nums[i]=0;
-        //     i++;
-        // }
         System.out.println(Arrays.toString(nums));
-        
-//         int i = 0; // slow pointer
-//         int j = 0; // fast pointer
-// //        int[] nums = {0, 1, 0, 3, 12};
-//         // Traverse through the array
-//         while (j < nums.length) {
-//             // If the current element is not zero, move it to the front of the array
-//             if (nums[j] != 0) {
-//                 nums[i] = nums[j];
-//                 i++;
-//             }
-//             j++;
-//         }
-
-//         // Fill the remaining elements with zeroes
-//         while (i < nums.length) {
-//             nums[i] = 0;
-//             i++;
-//         }
-//         System.out.println(Arrays.toString(nums));
+    
     }
+        //   int size = 0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         size++;
+        //     }else if(size>0){
+        //         int t = nums[i];
+        //         nums[i] = 0;
+        //         nums[i-size] = t;
+        //     }
+        // }
 }
