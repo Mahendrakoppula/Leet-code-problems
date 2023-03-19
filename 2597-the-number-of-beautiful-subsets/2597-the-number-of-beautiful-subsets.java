0287135323c74b5a828e13527ab381867a7e00c0
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-19 09:55:11 +0530

    Time: 683 ms (40.00%), Space: 42.8 MB (40.00%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int beautifulSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> seen = new HashMap<>();
+        int n = nums.length;
+        return dfs(nums, k, seen, n, 0) - 1;
+    }
+    
+    private int dfs(int[] nums, int k, Map<Integer, Integer> seen, int n, int i) {
+        if (i == n) {
+            return 1;
+        }
+        int res = 0;
+        if (seen.getOrDefault(nums[i] - k, 0) == 0) {
+            seen.put(nums[i], seen.getOrDefault(nums[i], 0) + 1);
+            res += dfs(nums, k, seen, n, i + 1);
+            seen.put(nums[i], seen.get(nums[i]) - 1);
+        }
+        res += dfs(nums, k, seen, n, i + 1);
+        return res;
+    }
+}
+
+
+
+
+// class Solution {
+//     // public int beautifulSubsets(int[] nums, int k) {
+//     //     int n = nums.length;
+//     // int[][] dp = new int[n+1][k];
+//     // for (int i = 0; i < k; i++) {
+//     //     dp[0][i] = 1;
+//     // }
+//     // for (int i = 1; i <= n; i++) {
+//     //     for (int j = 0; j < k; j++) {
+//     //         dp[i][j] = dp[i-1][j];
+//     //         if (j >= nums[i-1]) {
+//     //             dp[i][j] += dp[i-1][j-nums[i-1]];
+//     //         }
+//     //         if (j+nums[i-1] < k) {
+//     //             dp[i][j] += dp[i-1][j+nums[i-1]];
+//     //         }
+//     //     }
+//     // }
+//     // int ans = 0;
+//     // for (int i = 0; i < k; i++) {
+//     //     ans += dp[n][i];
+//     // }
+//     // return ans;
+//     // }
+//        public int numOfBeautifulSubsets(int[] nums, int k) {
+//     int n = nums.length;
+//     int ans = 0;
+//     Arrays.sort(nums);
+
+//     // dp[i] stores the number of beautiful subsets that end with nums[i]
+//     int[] dp = new int[n];
+
+//     for (int i = 0; i < n; i++) {
+//         dp[i] = 1; // every number forms a beautiful subset with itself
+//         for (int j = 0; j < i; j++) {
+//             if (nums[i] - nums[j] != k) {
+//                 dp[i] += dp[j]; // add the number of beautiful subsets that end with nums[j]
+//             }
+//         }
+//         ans += dp[i]; // add the number of beautiful subsets that end with nums[i]
+//     }
+//     return ans;
+
+//     }
+// }
