e7fab566cae9ad07ac1158c5c4411579bddab5af
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-20 20:29:27 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (41.80%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,12 +1,28 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
-    for(int i=0; i<flowerbed.length && count<n; i++) {
-        if(flowerbed[i] == 0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)) {
-            flowerbed[i] = 1;
-            count++;
+    //     int count = 0;
+    // for(int i=0; i<flowerbed.length && count<n; i++) {
+    //     if(flowerbed[i] == 0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)) {
+    //         flowerbed[i] = 1;
+    //         count++;
+    //     }
+    // }
+    // return count == n;
+        
+         int total = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            boolean isLeftPlotEmpty = (i == 0) || flowerbed[i-1] == 0;
+            if (flowerbed[i] == 0) {
+                boolean isRightPlotEmpty = (i == flowerbed.length-1) || flowerbed[i+1] == 0;
+                if (isLeftPlotEmpty && isRightPlotEmpty) {
+                    flowerbed[i] = 1;
+                    total++;
+                    if (total >= n) {
+                        return true;
+                    }
+                }
+            }
         }
-    }
-    return count == n;
+        return total >= n;
     }
 }
