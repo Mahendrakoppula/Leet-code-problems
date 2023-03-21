e2b08f4e228d8b8832e7fc6c3d63fb368101e710
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-21 22:57:55 +0530

    Time: 3 ms (7.17%), Space: 42.7 MB (28.20%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,12 +15,42 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-         if (root == null) {
-            return false;
-        }
-        if (root.left == null && root.right == null && targetSum == root.val) {
+        //  if (root == null) {
+        //     return false;
+        // }
+        // if (root.left == null && root.right == null && targetSum == root.val) {
+        //     return true;
+        // }
+        // return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        
+        if (root == null) {
+        return false;
+    }
+    
+    Stack<TreeNode> stack = new Stack<>();
+    Stack<Integer> sumStack = new Stack<>();
+    stack.push(root);
+    sumStack.push(targetSum - root.val);
+    
+    while (!stack.isEmpty()) {
+        TreeNode curr = stack.pop();
+        int currSum = sumStack.pop();
+        
+        if (curr.left == null && curr.right == null && currSum == 0) {
             return true;
         }
-        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        
+        if (curr.right != null) {
+            stack.push(curr.right);
+            sumStack.push(currSum - curr.right.val);
+        }
+        
+        if (curr.left != null) {
+            stack.push(curr.left);
+            sumStack.push(currSum - curr.left.val);
+        }
+    }
+    
+    return false;
     }
 }
