ef85834378e1a6a45f57cc04fa4eb903da2e2651
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-22 23:15:20 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (23.46%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -8,15 +8,15 @@ class Solution {
         int mid=lo+(hi-lo)/2;
          if(nums[mid]==target){
             return mid;
-        // }else if(target>nums[mid]){
+        }else if(nums[mid]>target){
             
              
-         }else if (nums[mid] < target){
-                // hi=mid-1;
-                 lo=mid+1;
+         // }else if (nums[mid] < target){
+                hi=mid-1;
+                 // lo=mid+1;
             }else{
-                // lo=mid+1;
-             hi=mid-1;
+                lo=mid+1;
+             // hi=mid-1;
             }
         }
         return -1;
