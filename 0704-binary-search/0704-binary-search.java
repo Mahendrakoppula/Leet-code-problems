aa545ea6c7de945f0091b039c22548fa0d5768d5
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-22 23:14:12 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (56.58%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,19 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // int lo=0;
-        // int hi=nums.length-1;
-        // int mid=lo+(hi-lo)/2;
-        // if(nums[mid]==target){
-        //     return mid;
-        // }
-        // while(lo<=hi){
-        //     if(target>nums[mid]){
-        //         hi=mid-1;
-        //     }else{
-        //         lo=mid+1;
-        //     }
-        // }
-        // return -1;
+        int lo=0;
+        int hi=nums.length-1;
+        
+       
+        while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+         if(nums[mid]==target){
+            return mid;
+        // }else if(target>nums[mid]){
+            
+             
+         }else if (nums[mid] < target){
+                // hi=mid-1;
+                 lo=mid+1;
+            }else{
+                // lo=mid+1;
+             hi=mid-1;
+            }
+        }
+        return -1;
         
         // for(int p: nums){
         //     if(nums[p]==target){
@@ -22,11 +28,11 @@ class Solution {
         // }
         // return -1;
         
-        for(int i=0;i<nums.length;i++){
-             if(nums[i]==target){
-                 return i;
-             }
-         }
-        return -1;
+        // for(int i=0;i<nums.length;i++){
+        //      if(nums[i]==target){
+        //          return i;
+        //      }
+        //  }
+        // return -1;
     }
 }
