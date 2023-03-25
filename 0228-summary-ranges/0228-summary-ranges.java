5de54cd2ce24bbf1e14ece032591ea78c14c0615
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-25 23:38:01 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (16.96%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,89 @@
+class Solution {
+  public List<String> summaryRanges(int[] nums) {
+    // List<String> result = new ArrayList<>();
+    // if (nums == null || nums.length == 0) {
+    //     return result;
+    // }
+    // int start = nums[0];
+    // int end = nums[0];
+    // for (int i = 1; i < nums.length; i++) {
+    //     if (nums[i] == end + 1) {
+    //         end = nums[i];
+    //     } else {
+    //         if (start == end) {
+    //             result.add(Integer.toString(start));
+    //         } else {
+    //             result.add(start + "->" + end);
+    //         }
+    //         start = nums[i];
+    //         end = nums[i];
+    //     }
+    // }
+    // if (start == end) {
+    //     result.add(Integer.toString(start));
+    // } else {
+    //     result.add(start + "->" + end);
+    // }
+    // return result;
+
+    //  List<String> result = new ArrayList<>();
+    //     if (nums.length < 1) {
+    //         return result;
+    //     }
+    //     int start = nums[0];
+    //     int end = nums[0];
+    //     boolean isRange = false;
+    //     for (int i = 1; i < nums.length; i++) {
+    //         if (nums[i] == end + 1) {
+    //             end = nums[i];
+    //             isRange = true;
+    //         } else {
+    //             if (isRange) {
+    //                 result.add(start + "->" + end);
+    //                 isRange = false;
+    //             } else {
+    //                 result.add(Integer.toString(start));
+    //             }
+    //             start = nums[i];
+    //             end = nums[i];
+    //         }
+    //     }
+    //     if (isRange) {
+    //         result.add(start + "->" + end);
+    //     } else {
+    //         result.add(Integer.toString(start));
+    //     }
+    //     return result;
+
+       List<String> list = new ArrayList<>();
+        if (nums.length == 0) return list;
+        int start = nums[0], end = nums[0];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] + 1 != nums[i + 1]) {
+                if (start == end) {
+                    sb.append(start);
+                    list.add(sb.toString());
+                } else {
+                    sb.append(start).append("->").append(end);
+                    list.add(sb.toString());
+                }
+                sb.setLength(0);
+                start = nums[i + 1];
+                end = nums[i + 1];
+            } else {
+                end = nums[i + 1];
+            }
+        }
+        if (start == end) {
+            sb.append(start);
+            list.add(sb.toString());
+        } else {
+            sb.append(start).append("->").append(end);
+            list.add(sb.toString());
+        }
+        return list;
+}
+
+
+}
