e31899c3663fcdd206846038f0e9525ceb072819
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-25 23:40:43 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (84.99%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -55,34 +55,76 @@ class Solution {
     //     }
     //     return result;
 
-       List<String> list = new ArrayList<>();
-        if (nums.length == 0) return list;
-        int start = nums[0], end = nums[0];
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] + 1 != nums[i + 1]) {
-                if (start == end) {
-                    sb.append(start);
-                    list.add(sb.toString());
-                } else {
-                    sb.append(start).append("->").append(end);
-                    list.add(sb.toString());
-                }
-                sb.setLength(0);
-                start = nums[i + 1];
-                end = nums[i + 1];
-            } else {
-                end = nums[i + 1];
-            }
-        }
-        if (start == end) {
-            sb.append(start);
-            list.add(sb.toString());
-        } else {
+       // List<String> list = new ArrayList<>();
+       //  if (nums.length == 0) return list;
+       //  int start = nums[0], end = nums[0];
+       //  StringBuilder sb = new StringBuilder();
+       //  for (int i = 0; i < nums.length - 1; i++) {
+       //      if (nums[i] + 1 != nums[i + 1]) {
+       //          if (start == end) {
+       //              sb.append(start);
+       //              list.add(sb.toString());
+       //          } else {
+       //              sb.append(start).append("->").append(end);
+       //              list.add(sb.toString());
+       //          }
+       //          sb.setLength(0);
+       //          start = nums[i + 1];
+       //          end = nums[i + 1];
+       //      } else {
+       //          end = nums[i + 1];
+       //      }
+       //  }
+       //  if (start == end) {
+       //      sb.append(start);
+       //      list.add(sb.toString());
+       //  } else {
+       //      sb.append(start).append("->").append(end);
+       //      list.add(sb.toString());
+       //  }
+       //  return list;
+      
+      
+      List<String> result = new ArrayList<>();
+if (nums.length < 1) {
+    return result;
+}
+
+int start = nums[0];
+int end = nums[0];
+boolean isRange = false;
+StringBuilder sb = new StringBuilder();
+
+for (int i = 1; i < nums.length; i++) {
+    if (nums[i] == end + 1) {
+        end = nums[i];
+        isRange = true;
+    } else {
+        if (isRange) {
             sb.append(start).append("->").append(end);
-            list.add(sb.toString());
+            result.add(sb.toString());
+            sb.setLength(0);
+            isRange = false;
+        } else {
+            sb.append(start);
+            result.add(sb.toString());
+            sb.setLength(0);
         }
-        return list;
+        start = nums[i];
+        end = nums[i];
+    }
+}
+
+if (isRange) {
+    sb.append(start).append("->").append(end);
+    result.add(sb.toString());
+} else {
+    sb.append(start);
+    result.add(sb.toString());
+}
+
+return result;
+
 }
 
 
