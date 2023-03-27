62bfddc37a90839cf08fba06e33aeea002cc4c20
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-27 20:26:10 +0530

    Time: 1 ms (83.65%), Space: 40.1 MB (74.14%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+       String[] words = s.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(words[i])) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(words[i])) {
+                    return false;
+                }
+                map.put(c, words[i]);
+            }
+        }
+        return true;
+    }
+}
