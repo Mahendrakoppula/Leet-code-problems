ad500ad10bdca2d3e07bb6786f6bf3474ff48940
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-03-29 22:57:16 +0530

    Time: 5 ms (63.45%), Space: 60 MB (53.80%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -12,24 +12,40 @@ class Solution {
         
 //         return -1; 
         
-         int n = nums.length;
-        int left = 1, right = n - 1, duplicate = -1;
+//          int n = nums.length;
+//         int left = 1, right = n - 1, duplicate = -1;
         
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int count = 0;
-            for (int num : nums) {
-                if (num <= mid) {
-                    count++;
-                }
-            }
-            if (count <= mid) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-                duplicate = mid;
-            }
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             int count = 0;
+//             for (int num : nums) {
+//                 if (num <= mid) {
+//                     count++;
+//                 }
+//             }
+//             if (count <= mid) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//                 duplicate = mid;
+//             }
+//         }
+//         return duplicate;
+        
+          int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return duplicate;
+        
+        return slow;
     }
 }
