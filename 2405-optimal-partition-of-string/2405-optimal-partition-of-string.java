92f54b21f1a60be5766f290f859be8dd8d2925cf
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-04 22:26:09 +0530

    Time: 11 ms (85.92%), Space: 43.3 MB (53.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,20 +1,32 @@
 class Solution {
     public int partitionString(String s) {
-        Set<Character>set= new HashSet<>();
-      int  count=0;
-        for(char c: s.toCharArray()){
-            if(set.contains(c)){
-                count++;
+//         Set<Character>set= new HashSet<>();
+//       int  count=0;
+//         for(char c: s.toCharArray()){
+//             if(set.contains(c)){
+//                 count++;
                 
-                set= new HashSet<>();
-                set.add(c);
-            }else{
-                set.add(c);
+//                 set= new HashSet<>();
+//                 set.add(c);
+//             }else{
+//                 set.add(c);
+//             }
+//         }
+//         if(!set.isEmpty()){
+//             count++;
+//         }
+//         return count;
+        
+        int[] arr = new int[26];
+        int ans = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(arr[ch-'a']>0){
+                ans++;
+                arr = new int[26];
             }
+            arr[ch-'a']++;
         }
-        if(!set.isEmpty()){
-            count++;
-        }
-        return count;
+        return ans + 1;
     }
 }
