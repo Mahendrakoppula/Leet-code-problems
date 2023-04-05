c77cdbf870dd5e989716820e976fc3e78b7444b1
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-05 12:52:34 +0530

    Time: 18 ms (31.90%), Space: 50.7 MB (57.00%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,12 +1,13 @@
 class Solution {
-    public int numRescueBoats(int[] nums, int limit) {
-//          int i=0;
+    public int numRescueBoats(int[] people, int limit) {
+//         Arrays.sort(nums);
+//         int i=0;
 //         int j= nums.length-1;
 //         int boats=0;
 //         while(i<=j){
-//             if(i==j && nums[i]<=limit && nums[j]<=limit){
-//                 boats++;
-//             }
+//             // if(i==j && nums[i]<=limit && nums[j]<=limit){
+//             //     boats++;
+//             // }
 //             if(nums[i]==limit){
 //                 boats++;
 //                 i++;
@@ -25,18 +26,66 @@ class Solution {
 //         }
 //         return boats;
         
-        Arrays.sort(nums);
-        int boatCount = 0, left = 0, right = nums.length - 1;
-while(left <= right){
-if(nums[left] + nums[right] <= limit){
-left++;
-right--;
-}
-else{
-right--;
-}
-boatCount++;
-}
-return boatCount;
+        // Arrays.sort(nums);
+        // int boatCount = 0, left = 0, right = nums.length ;
+// while(left <= right){
+// if(nums[left] + nums[right] <= limit){
+// left++;
+// right--;
+// }
+// else{
+// right--;
+// }
+// boatCount++;
+// }
+// return boatCount;
+//     }
+        
+//       int boatCount = 0, left = 0, right = nums.length-1;
+//         while(left <= right){
+//             if(nums[right]==limit){
+//                 boatCount++;
+//                 right--;
+//             }else if(nums[left]==limit){
+//                 boatCount++;
+//                 left++;
+//             }else if(nums[left] + nums[right] <= limit){
+//                 left++;
+//                 right--;
+//                 boatCount++;
+//             }
+//             else{
+//                 right--;
+//                 boatCount++;
+//             }
+
+//         }
+//         return boatCount;
+        
+           if (people == null || people.length == 0 || limit <= 0) {
+        return 0; // edge case: empty or null array, or invalid limit
     }
+
+    int boatCount = 0, left = 0, right = people.length - 1;
+    Arrays.sort(people); // sort the people by weight
+
+    while (left <= right) {
+        if (people[right] == limit) {
+            boatCount++;
+            right--;
+        } else if (people[left] == limit) {
+            boatCount++;
+            left++;
+        } else if (people[left] + people[right] <= limit) {
+            left++;
+            right--;
+            boatCount++;
+        } else {
+            right--;
+            boatCount++;
+        }
+    }
+
+    return boatCount;
+}
 }
