d8aec68bde54cba56cf98811dea81f844957216a
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-05 15:27:40 +0530

    Time: 1 ms (99.76%), Space: 41 MB (22.75%) - LeetHub

--- 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
@@ -2,26 +2,23 @@
 
 class Solution {
     public int minNumber(int[] nums1, int[] nums2) {
-     // int s1 = Integer.MAX_VALUE;
-     //     int s2=Integer.MAX_VALUE;
-
+    
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-
-        HashSet<Integer> set = new HashSet<>();
-        for(int i:nums2)
-        {
+        
+        Set<Integer> set= new HashSet<>();
+        for(int i:nums1){
             set.add(i);
         }
-
-        for(int i:nums1)
-        {
-            if(set.contains(i)) return i;
+        for(int i: nums2){
+            if(set.contains(i)){
+                return i;
+            }
         }
 
-         int ans1 = nums1[0]*10 + nums2[0];
-        int ans2 = nums2[0]*10 + nums1[0];
-
-        return Math.min(ans1,ans2);
+        int ans=nums1[0]*10+nums2[0];
+        int ans1= nums2[0]*10+nums1[0];
+        
+        return Math.min(ans,ans1);
     }
 }
