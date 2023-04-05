b8b1fb8cd8c8e6c90086144d3e59328e3a5bf780
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-05 14:39:45 +0530

    Time: 1 ms (99.88%), Space: 40.7 MB (16.17%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        //  int[] items = new int[numOnes+numZeros+numNegOnes];
+        // int idx = 0;
+        // for(int i=0; i<numOnes; i++) {
+        //     items[idx++] = 1;
+        // }
+        // for(int i=0; i<numZeros; i++) {
+        //     items[idx++] = 0;
+        // }
+        // for(int i=0; i<numNegOnes; i++) {
+        //     items[idx++] = -1;
+        // }
+        // Arrays.sort(items);
+        // int sum = 0;
+        // for(int i=items.length-1; i>=items.length-k; i--) {
+        //     sum += items[i];
+        // }
+        // return sum;
+        
+        
+//        int ans = 0;
+// if (numOnes <= k) {
+//     ans += numOnes;
+//     k -= numOnes;
+// } else {
+//     ans += k;
+//     k = 0;
+// }
+
+// if (k == 0) {
+//     return ans;
+// }
+
+// if (numZeros <= k) {
+//     k -= numZeros;
+// } else {
+//     k = 0;
+// }
+
+// if (k == 0) {
+//     return ans;
+// }
+
+// if (numNegOnes <= k) {
+//     ans -= numNegOnes;
+// } else {
+//     ans -= k;
+// }
+
+// return ans;
+        
+        // int ans=0;
+        // if(k<numOnes){
+        //     return k;
+        // }else if(k>numOnes){
+        //     ans+=numOnes;
+        //     k-=numOnes;
+        // }
+        return Math.min(k,numOnes)-Math.max(0,k-numOnes-numZeros);
+    }
+}
