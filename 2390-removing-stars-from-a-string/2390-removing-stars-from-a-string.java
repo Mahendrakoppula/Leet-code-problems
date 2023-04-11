93dc60fd81a5036c806490f7e40e3fd692dc0b7a
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-11 23:48:48 +0530

    Time: 17 ms (97.41%), Space: 43.4 MB (84.49%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -17,18 +17,30 @@ class Solution {
 //         }
 //         return sb.toString();
         
-         StringBuilder sb = new StringBuilder();
-    int count = 0;
-    for (int i = 0; i < s.length(); i++) {
-        char ch = s.charAt(i);
+    //      StringBuilder sb = new StringBuilder();
+    // int count = 0;
+    // for (int i = 0; i < s.length(); i++) {
+    //     char ch = s.charAt(i);
+    //     if (ch != '*') {
+    //         sb.append(ch);
+    //         count++;
+    //     } else if (count > 0) {
+    //         sb.deleteCharAt(count - 1);
+    //         count--;
+    //     }
+    // }
+    // return sb.toString();
+        
+        int count = 0;
+    char[] chars = s.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+        char ch = chars[i];
         if (ch != '*') {
-            sb.append(ch);
-            count++;
+            chars[count++] = ch;
         } else if (count > 0) {
-            sb.deleteCharAt(count - 1);
             count--;
         }
     }
-    return sb.toString();
+    return new String(chars, 0, count);
     }
 }
