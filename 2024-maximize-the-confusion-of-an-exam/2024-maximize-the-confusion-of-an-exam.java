22c1e4ef5c752b8ffc802ebd0ad2bbb5c41bda62
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-17 00:13:51 +0530

    Time: 26 ms (17.58%), Space: 42.7 MB (84.55%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,32 +1,32 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-         int n = answerKey.length();
-        int maxCount = 0;
-        int countT = 0, countF = 0;
-        Queue<Character> queue = new LinkedList<>();
+//          int n = answerKey.length();
+//         int maxCount = 0;
+//         int countT = 0, countF = 0;
+//         Queue<Character> queue = new LinkedList<>();
         
-        for (int i = 0; i < n; i++) {
-            char c = answerKey.charAt(i);
-            queue.offer(c);
-            if (c == 'T') {
-                countT++;
-            } else {
-                countF++;
-            }
+//         for (int i = 0; i < n; i++) {
+//             char c = answerKey.charAt(i);
+//             queue.offer(c);
+//             if (c == 'T') {
+//                 countT++;
+//             } else {
+//                 countF++;
+//             }
             
-            while (Math.min(countT, countF) > k) {
-                char removed = queue.poll();
-                if (removed == 'T') {
-                    countT--;
-                } else {
-                    countF--;
-                }
-            }
+//             while (Math.min(countT, countF) > k) {
+//                 char removed = queue.poll();
+//                 if (removed == 'T') {
+//                     countT--;
+//                 } else {
+//                     countF--;
+//                 }
+//             }
             
-            maxCount = Math.max(maxCount, queue.size());
-        }
+//             maxCount = Math.max(maxCount, queue.size());
+//         }
         
-        return maxCount;
+//         return maxCount;
         
 //           int n = answerKey.length();
 //         int maxCount = 0;
@@ -57,56 +57,56 @@ class Solution {
 //         return maxCount;
         
         
-//         int n = answerKey.length();
-//         int maxCount = 0;
+        int n = answerKey.length();
+        int maxCount = 0;
         
-//         // Counting consecutive T's
-//         int countT = 0;
-//         for (int i = 0; i < n; i++) {
-//             if (answerKey.charAt(i) == 'T') {
-//                 countT++;
-//             } else {
-//                 countT = 0;
-//             }
-//             maxCount = Math.max(maxCount, countT);
-//         }
+        // Counting consecutive T's
+        int countT = 0;
+        for (int i = 0; i < n; i++) {
+            if (answerKey.charAt(i) == 'T') {
+                countT++;
+            } else {
+                countT = 0;
+            }
+            maxCount = Math.max(maxCount, countT);
+        }
         
-//         // Counting consecutive F's
-//         int countF = 0;
-//         for (int i = 0; i < n; i++) {
-//             if (answerKey.charAt(i) == 'F') {
-//                 countF++;
-//             } else {
-//                 countF = 0;
-//             }
-//             maxCount = Math.max(maxCount, countF);
-//         }
+        // Counting consecutive F's
+        int countF = 0;
+        for (int i = 0; i < n; i++) {
+            if (answerKey.charAt(i) == 'F') {
+                countF++;
+            } else {
+                countF = 0;
+            }
+            maxCount = Math.max(maxCount, countF);
+        }
         
-//         // Adjusting for k
-//         if (k > 0) {
-//             countT = 0;
-//             countF = 0;
-//             int left = 0, right = 0;
-//             while (right < n) {
-//                 if (answerKey.charAt(right) == 'T') {
-//                     countT++;
-//                 } else {
-//                     countF++;
-//                 }
-//                 right++;
-//                 while (Math.min(countT, countF) > k) {
-//                     if (answerKey.charAt(left) == 'T') {
-//                         countT--;
-//                     } else {
-//                         countF--;
-//                     }
-//                     left++;
-//                 }
-//                 maxCount = Math.max(maxCount, right - left);
-//             }
-//         }
+        // Adjusting for k
+        if (k > 0) {
+            countT = 0;
+            countF = 0;
+            int left = 0, right = 0;
+            while (right < n) {
+                if (answerKey.charAt(right) == 'T') {
+                    countT++;
+                } else {
+                    countF++;
+                }
+                right++;
+                while (Math.min(countT, countF) > k) {
+                    if (answerKey.charAt(left) == 'T') {
+                        countT--;
+                    } else {
+                        countF--;
+                    }
+                    left++;
+                }
+                maxCount = Math.max(maxCount, right - left);
+            }
+        }
         
-//         return maxCount;
+        return maxCount;
         
 //            int n = answerKey.length();
 //         int left = 1, right = n;
