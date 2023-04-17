d25d482fd2e95ad39096e2b58dac6095a1135053
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-17 14:52:25 +0530

    Time: 1 ms (81.26%), Space: 42.4 MB (67.41%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//         List<Boolean>list= new ArrayList<>(candies.length);
+//         Collections.fill(list,true);
+//         int min=arr[0];
+//         // for(int i=0;i<=candies.length-1;i++){
+//         //     candies[i]=candies[i]+extraCandies;
+//         // }
+        
+//    for (int i = 0; i <candies.length; i++) {
+//         if (candies[i] < min) {
+//             min = candies[i];
+//             list.set(i,false);
+//         }
+//     }
+//         return list;
+        
+        List<Boolean> list = new ArrayList<>();
+       for (int i=0;i<candies.length;i++){
+           list.add(true);
+       }
+        int min = candies[0];
+
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > min) {
+                min = candies[i];
+//                list.set(i, false);
+            }
+        }
+                for (int i = 0; i <= candies.length - 1; i++) {
+
+            candies[i] = candies[i] + extraCandies;
+                    if (candies[i]<min){
+                        list.set(i,false);
+                    }
+        }
+        return list;
+        }
+    }
