c92aa3b931a000bc9b33ffbed6ac460767494645
Mahendra koppula <87843354+Mahendrakoppula@users.noreply.github.com>
2023-04-28 23:25:39 +0530

    Time: 2191 ms (5.08%), Space: 58.6 MB (75.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,19 +10,49 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-          ArrayList<Integer> al= new ArrayList<>();
+          // ArrayList<Integer> al= new ArrayList<>();
 
-        while(head!=null){
-            al.add(head.val);
-            head=head.next;
-        }
-        System.out.println(al);
-        int n=al.size();
-        for (int i = 0; i < n/2; i++) {
-            if(al.get(i)!=al.get(n-i-1)){
-                return false;
-            }
+ //        while(head!=null){
+ //            al.add(head.val);
+ //            head=head.next;
+ //        }
+ //        System.out.println(al);
+ //        int n=al.size();
+ //        for (int i = 0; i < n/2; i++) {
+ //            if(al.get(i)!=al.get(n-i-1)){
+ //                return false;
+ //            }
+ //    }
+ // return true;       
+        
+//          Stack<Integer>st= new Stack<>();
+//         while(head!=null){
+// //            al.add(head.value);
+//             if( st.isEmpty()){
+
+//                 st.push(head.val);
+//             }else if (head.val==st.peek()){
+//                 st.pop();
+//             }else {
+//             st.push(head.val);
+//         }
+//             head=head.next;
+//         }
+//         if(st.empty()||st.size() == 1){
+//             return true;
+//         }
+//         return false;
+        
+        ArrayList<Integer> original = new ArrayList<>();
+    ArrayList<Integer> reversed = new ArrayList<>();
+    while (head != null) {
+        original.add(head.val);
+        reversed.add(0, head.val);
+        head = head.next;
     }
- return true;       
+    if (original.equals(reversed)) {
+        return true;
+    } 
+        return false;
 }
 }
